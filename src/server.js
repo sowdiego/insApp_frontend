7e49d4ee5cacc8a9aa667c47d1a8f175a2aea1d1
sowdiego.ms@gmail.com <sowdiego.ms@gmail.com>
2019-01-05 15:58:32 +0000

    commit

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/ins-app-frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ins-app-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
